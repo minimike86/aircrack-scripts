583ee044921a5450b73e82dea67db1ddd94786c1
Michael Warner <minimike86@users.noreply.github.com>
2020-01-20 21:36:07 +0000

    Update 2. airodump-ng.sh

--- 2. airodump-ng.sh
+++ 2. airodump-ng.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 echo "\e[96mcapturing packets from monitor device:\e[0m"
-#airodump-ng wlan0mon -w "/root/Downloads/captures/"$(date +%F_%H-%M-%S)"_cap"
+
+# Listen to all APs within range and log the output.
 airodump-ng wlan0mon -w "/root/Downloads/captures/"$(date +%F_%H-%M-%S)"_cap"
+
 sleep 5
