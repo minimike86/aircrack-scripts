98dac7085fa222d2df4c1a4405e17696fedd0a30
Michael Warner <minimike86@users.noreply.github.com>
2020-01-20 21:35:23 +0000

    Update 2. airodump-ng (RogueAP).sh

--- 2. airodump-ng (RogueAP).sh
+++ 2. airodump-ng (RogueAP).sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 echo "\e[96mcapturing packets from monitor device:\e[0m"
 
-# Change the channel to from 1 "-c 1" to whatever your AP is using if necessary!
+# Change the channel to from 1 "-c 1" to whatever number your target AP is using
+# Change --bssid to match the MAC address of the target AP
 airodump-ng wlan0mon -c 1 --bssid 1C:23:2C:B7:D0:29 -w "/root/Downloads/captures/"$(date +%F_%H-%M-%S)"_cap"
 
 #airodump-ng wlan0mon -w "/root/Downloads/captures/"$(date +%F_%H-%M-%S)"_cap"
